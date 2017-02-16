3e0e60160cddf9d01c904fcb7029787b4337afcd
mjoulel <joulelm@gmail.com>
2017-02-16 01:39:09 -0600

    Add files via upload

---
+++ week-14-burger/db/schema.sql
@@ -0,0 +1,14 @@
+-- Create & use burgers_db
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+-- Create a burgers table with id: an auto incrementing int that serves as the primary key, burger_name: a string, devoured: a boolean, date: a TIMESTAMP fields
+CREATE TABLE burgers
+(
+id int NOT NULL AUTO_INCREMENT,
+burger_name varchar(255) NOT NULL,
+devoured boolean(10) NOT NULL,
+date TIMESTAMP(8) NOT NULL,
+PRIMARY KEY (id)
+);
+
---
+++ week-14-burger/server.js
@@ -0,0 +1,12 @@
+// Marie Laborde
+// 2/12/2017
+// Week 14 - Burger
+
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var exphbs = require("express-handlebars");
+var methodOverride = require("method-override");
+var mysql = require("mysql");
+
